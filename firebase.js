2acac511a4e195291ef2932a87b7f510ddd05f54
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2021-08-06 13:35:35 -0400

    integrated firebase with the app

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+// Firebase Connection
+const firebaseConfig = {
+  apiKey: 'AIzaSyBEO0WuPZb4SBKCRLtZCr8Q7nZ7ogEwTUk',
+  authDomain: 'documaker-6d0d2.firebaseapp.com',
+  projectId: 'documaker-6d0d2',
+  storageBucket: 'documaker-6d0d2.appspot.com',
+  messagingSenderId: '702876927649',
+  appId: '1:702876927649:web:da67ff2899eb4fe34552e0',
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export { db };
